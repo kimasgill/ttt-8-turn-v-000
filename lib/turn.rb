f8ff4ebeaad3f980af1aad94becd9ef8081a4599
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-21 19:21:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,42 +1,31 @@
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.chomp 
-  index = input_to_index(user_input)
-   if valid_move?(board,index)
-     move(board, index, value="X")
-     display_board(board)
-   else
-     puts "Please enter 1-9:"
-     gets.chomp
-   end
-end
-  
-
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} " 
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} " 
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} " 
-end
-
-
-def valid_move?(board,index)
-  !position_taken?(board, index) && index.between?(0, 8)
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
   else
-    return true
+    true
   end
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 def input_to_index(user_input)
    user_input.to_i - 1
 end
 
-def move(board, index, value="X")
+def move(board, index, value = 'X')
   board[index] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
 end
